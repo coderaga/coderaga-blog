1e2f83bbec8823bf6e68621686e6b470fdc3caff
voidzero <kartikjagdale11@gmail.com>
2016-11-06 02:10:02 +0530

    First Commit

---
+++ db/migrate/20140911142836_create_posts.rb
@@ -0,0 +1,9 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string    :title, null: false, default: ""
+      t.text      :content, null: false, default: ""
+      t.timestamps
+    end
+  end
+end
